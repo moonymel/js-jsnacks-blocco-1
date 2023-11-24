1e1503a356e31b1f4710b3d7a5a96ca0ac2c30db
moonymel <musumeci.me@gmail.com>
2023-11-24 17:29:36 +0100

    bonus array-tail completato

--- array-tail/js/script.js
+++ array-tail/js/script.js
@@ -14,6 +14,18 @@ for (let i=0; i<elementi; i++) {
   
 }  
 
-for(let i=elementi-1; i>array.length-6; i--) {
-        console.log(array[i]);
+// for(let i=elementi-1; i>array.length-6; i--) {
+//         console.log(array[i]);
+//     }
+
+let stamp = parseInt(prompt('Quanti di questi elementi stampo in console?'));
+
+for(let i=0; i<stamp ; i++) {
+    if(stamp > elementi) {
+        console.log('erroreee');
     }
+
+    else {
+        console.log(array[i])
+    }
+}
